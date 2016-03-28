754ba03a36b429ebac63d8685042b5888c2ae617
Rogatnev Nikita <mail@rogatnev.ru>
2016-03-28 18:33:55 +0300

    Button to a

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,38 @@
+/*
+Template Name: Responsive Animated 404 Pages
+Author: Rogatnev Nikita
+Version: 1.0.0
+Tags: 
+*/
+
+
+/* ------------------------------------------ */
+/*             MODAL BOX
+/* ------------------------------------------ */
 $(document).on('ready', function() {
-  $modal = $('.modal');
-  $overlay = $('.modal-overlay');
-  $modal.bind('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(e) {
-    if ($modal.hasClass('modal--leave')) {
-      $modal.removeClass('modal--leave');
-    }
-  });
-
-  $('.modal__close').on('click', function() {
-    $overlay.removeClass('modal--show');
-    $modal.removeClass('modal--appear').addClass('modal--leave');
-  });
-
-  $('.modal--open').on('click', function() {
-    $overlay.addClass('modal--show');
-    $modal.removeClass('modal--leave').addClass('modal--appear');
-  });
-
+	modal = $('.modal');
+	var modalbox = document.querySelector(".modal");
+	overlay = $('.modal-overlay');
+	modal.bind('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(e) {
+		if (modal.hasClass('modal--leave')) {
+			modal.removeClass('modal--leave');
+		}
+	});
+	$('.modal__close').on('click', function() {
+		overlay.removeClass('modal--show');
+		modal.removeClass('modal--appear').addClass('modal--leave');
+	});
+	$(document).keydown(function(e) {
+		if (e.keyCode === 27) {
+			if (modalbox.classList.contains('modal--appear')) {
+				overlay.removeClass('modal--show');
+				modal.removeClass('modal--appear').addClass('modal--leave');
+			}
+		}
+	});
+	$('.modal--open').on('click', function() {
+		event.preventDefault();
+		overlay.addClass('modal--show');
+		modal.removeClass('modal--leave').addClass('modal--appear');
+	});
 });
