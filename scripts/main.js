d4869612ce241299de0b14cd6d1cf6bc1686e75d
Rogatnev Nikita <mail@rogatnev.ru>
2016-03-28 16:52:07 +0300

    Report form

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,20 @@
+$(document).on('ready', function() {
+  $modal = $('.modal');
+  $overlay = $('.modal-overlay');
+  $modal.bind('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(e) {
+    if ($modal.hasClass('modal--leave')) {
+      $modal.removeClass('modal--leave');
+    }
+  });
 
+  $('.modal__close').on('click', function() {
+    $overlay.removeClass('modal--show');
+    $modal.removeClass('modal--appear').addClass('modal--leave');
+  });
+
+  $('.modal--open').on('click', function() {
+    $overlay.addClass('modal--show');
+    $modal.removeClass('modal--leave').addClass('modal--appear');
+  });
+
+});
