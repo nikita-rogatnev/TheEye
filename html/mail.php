5c36a69ffdd95f5c401e3d863ce9cbedbe6cda14
Rogatnev Nikita <mail@rogatnev.ru>
2016-04-10 22:10:40 +0300

    Ajax form

--- html/mail.php
+++ html/mail.php
@@ -1,27 +1,37 @@
 <?php
 
+    $errors = array();
+    $data = array();
 
+    // if any of these variables don't exist, add an error to our $errors array
+    if (empty($_POST['email']))
+        $errors['email'] = 'Email is required.';
 
+    if (empty($_POST['text']))
+        $errors['text'] = 'Text is required.';
 
-if ( isset($_POST['email']) && isset($_POST['text']) && filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) ) {
-
-  // detect & prevent header injections
-  $test = "/(content-type|bcc:|cc:|to:)/i";
-  foreach ( $_POST as $key => $val ) {
-    if ( preg_match( $test, $val ) ) {
-      exit;
+    // if there are any errors in our errors array, return a success boolean of false
+    if ( ! empty($errors)) {
+        $data['errors']  = $errors;
     }
-  }
 
-  $to = "mail@rogatnev.ru"; //mail@yourdomain.com
+    else {
+        $to = 'mail@rogatnev.ru'; //mail@yourdomain.com
+        $subject = 'You have a message sent from your site';
+        $email = $_POST['email'];
+        $text = $_POST['text'];
+        $headers = 'From: $to';
+        $headers .= 'Reply-To: $email';
 
-  $headers = "From: $from";
-  $subject = "You have a message sent from your site";
-  $from = $_POST['email'];
-  $text = $_POST['text'];
+        // mail foem data
+        mail($to, $subject, $text, $headers);
 
-  //send email
-  $send = mail($to, $subject, $body, $headers);
+        // show a message of success and provide a true success variable
+        $data['success'] = true;
+        $data['message'] = 'Success!';
+    }
 
-}
+    // return all our data to an AJAX call
+    echo json_encode($data);
+
 ?>
--- html/scripts/main.js
+++ html/scripts/main.js
@@ -37,39 +37,59 @@ $(document).on('ready', function() {
     });
 
 
-    // Contact Form
-    $("#contact").submit(function(e) {
-        e.preventDefault();
-        var name = $("#name").val();
-        var email = $("#email").val();
-        var text = $("#text").val();
-        var dataString = 'name=' + name + '&email=' + email + '&text=' + text;
 
-        function isValidEmail(emailAddress) {
-            var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
-            return pattern.test(emailAddress);
+    /* ------------------------------------------ */
+    /*             CONTACT FORM
+    /* ------------------------------------------ */
+    $('form').submit(function(event) {
+
+        $('.modal__content-item').removeClass('modal__content-item--error'); // remove the error class
+        $('.modal__content-error-description').remove(); // remove the error text
+
+        // get the form data
+        var formData = {
+            'email': $('input[name=email]').val(),
+            'text': $('textarea[name=text]').val()
         };
 
-        if (isValidEmail(email) && (text.length > 100) && (name.length > 1)) {
-            $.ajax({
-                type: "POST",
-                url: "/wp-content/themes/YOURTHEMENAME/functions.php",
-                data: dataString,
-                success: function() {
-                    $('.success').fadeIn(1000);
+        // process the form
+        $.ajax({
+            type: 'POST',
+            url: 'mail.php',
+            data: formData,
+            dataType: 'json',
+            encode: true
+        })
+
+        // using the done promise callback
+        .done(function(data) {
+
+            // here we will handle errors and validation messages
+            if (!data.success) {
+
+                // handle errors for email
+                if (data.errors.email) {
+                    $('#email').addClass('modal__content-item--error'); // add the error class to show red input
+                    $('#email').prepend('<div class="modal__content-error-description">' + data.errors.email + '</div>'); // add the actual error message under our input
                 }
-            });
-        } else {
-            $('.error').fadeIn(1000);
-        }
 
-        return false;
+                // handle errors for text
+                if (data.errors.text) {
+                    $('#text').addClass('modal__content-item--error'); // add the error class to show red input
+                    $('#text').prepend('<div class="modal__content-error-description">' + data.errors.text + '</div>'); // add the actual error message under our input
+                }
+            }
+
+            else {
+
+                // ALL GOOD! just show the success message!
+                $('.modal__form').addClass('modal__form--success'); // add the error class to show red input
+                $('form').prepend('<div class="modal__content-item modal__content-item--success">' + data.message + '</div>');
+            }
+        })
+
+        // stop the form from submitting the normal way and refreshing the page
+        event.preventDefault();
     });
-
-
-
-
-
-
 
 });
