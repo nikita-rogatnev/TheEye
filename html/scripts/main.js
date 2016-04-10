8b8a85fee8dc536d39ed5e7bffa3e53e465eb55d
Rogatnev Nikita <mail@rogatnev.ru>
2016-04-10 20:32:44 +0300

    csscomb

--- html/scripts/main.js
+++ html/scripts/main.js
@@ -10,29 +10,29 @@ Tags: error, error page, creative, clean, modern, minimal, animated, responsive,
 /*             MODAL BOX
 /* ------------------------------------------ */
 $(document).on('ready', function() {
-	modal = $('.modal');
-	var modalbox = document.querySelector(".modal");
-	overlay = $('.modal-overlay');
-	modal.bind('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(e) {
-		if (modal.hasClass('modal--leave')) {
-			modal.removeClass('modal--leave');
-		}
-	});
-	$('.modal__close').on('click', function() {
-		overlay.removeClass('modal--show');
-		modal.removeClass('modal--appear').addClass('modal--leave');
-	});
-	$(document).keydown(function(e) {
-		if (e.keyCode === 27) {
-			if (modalbox.classList.contains('modal--appear')) {
-				overlay.removeClass('modal--show');
-				modal.removeClass('modal--appear').addClass('modal--leave');
-			}
-		}
-	});
-	$('.modal--open').on('click', function() {
-		event.preventDefault();
-		overlay.addClass('modal--show');
-		modal.removeClass('modal--leave').addClass('modal--appear');
-	});
+    modal = $('.modal');
+    var modalbox = document.querySelector(".modal");
+    overlay = $('.modal-overlay');
+    modal.bind('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(e) {
+        if (modal.hasClass('modal--leave')) {
+            modal.removeClass('modal--leave');
+        }
+    });
+    $('.modal__close').on('click', function() {
+        overlay.removeClass('modal--show');
+        modal.removeClass('modal--appear').addClass('modal--leave');
+    });
+    $(document).keydown(function(e) {
+        if (e.keyCode === 27) {
+            if (modalbox.classList.contains('modal--appear')) {
+                overlay.removeClass('modal--show');
+                modal.removeClass('modal--appear').addClass('modal--leave');
+            }
+        }
+    });
+    $('.modal--open').on('click', function() {
+        event.preventDefault();
+        overlay.addClass('modal--show');
+        modal.removeClass('modal--leave').addClass('modal--appear');
+    });
 });
